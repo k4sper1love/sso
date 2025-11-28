46d4790f70c69520ddf2e91682993bb200187d4b
Sergey <s_yelkin@proton.me>
2025-11-28 09:05:38 +0500

    feat: add server logic

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,80 @@
+package auth
+
+import (
+	"context"
+
+	ssov1 "github.com/k4sper1love/sso-protos/gen/go/sso"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	emptyValue = 0
+)
+
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appID int) (string, error)
+	Register(ctx context.Context, email string, password string) (int, error)
+	IsAdmin(ctx context.Context, userID int) (bool, error)
+}
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
+}
+
+func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+	
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	userID, err := s.auth.Register(ctx, req.GetEmail(), req.GetPassword()) 
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to register")
+	}
+
+	return &ssov1.RegisterResponse{UserId: int64(userID)}, nil
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	if req.GetUserId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, int(req.GetUserId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to check admin status")
+	}
+
+	return &ssov1.IsAdminResponse{IsAdmin: isAdmin}, nil
+}
